b11dff5529365d4ab5e676b088f9693f22fa6439
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-13 13:53:09 +0530

    Update Secondkid.java

--- Secondkid.java
+++ Secondkid.java
@@ -3,10 +3,10 @@ public class second {
 	public static void main(String[] args) {
 		Scanner word=new Scanner(System.in);
 		int N=word.nextInt();
-		if(N>=1 && N<=100000 && N%2==0){
+		if((N>=1 && N<=100000) && N%2==0){
           System.out.println("Even");
 	}
-		else if(N>=1 && N<=100000 && N%2!=0){
+		else if((N>=1 && N<=100000) && N%2!=0){
 			System.out.println("Odd");
 		}
 		else{
