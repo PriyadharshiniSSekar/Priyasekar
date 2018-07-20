52d745c611b5e11e43bf81a94301b55f8c170bf3
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-20 14:30:22 +0530

    Create eleven.java

---
+++ eleven.java
@@ -0,0 +1,9 @@
+public class EleventhBeg {
+
+	public static void main(String[] args) {
+        Scanner word=new Scanner(System.in);
+        int num=word.nextInt();
+        int K=word.nextInt();int s=(int)Math.pow(num,K);
+        System.out.println(s);
+	}
+}
