fd2b4f042204655ae074b0788615cacc371b4fab
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-20 12:31:20 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class seventhkid {
+
+	public static void main(String[] args) {
+		Scanner word=new Scanner(System.in);
+		int N=word.nextInt();
+		for(int i=0;i<N;i++){
+			System.out.println("Hello");
+		}
+	}
+}
