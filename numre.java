148b6a49abed723b32e39690f68640dc37ab4c4b
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-20 15:38:41 +0530

    Create numre.java

---
+++ numre.java
@@ -0,0 +1,9 @@
+public class rev {
+
+	public static void main(String[] args) {
+		Scanner word=new Scanner(System.in);
+		String s=word.next();
+		String v=new StringBuffer(s).reverse().toString();
+	   System.out.println(v);
+	}
+}
