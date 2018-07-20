6ae036b2df4d9674248e0b772c4699a52548ff9f
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-20 12:02:44 +0530

    Update alpha.java

--- alpha.java
+++ alpha.java
@@ -3,7 +3,7 @@ public class Fourthkid {
 	public static void main(String[] args) {
 		Scanner word=new Scanner(System.in);
 		char ch=word.next().charAt(0);
-		if((ch >='a' && ch<='z')||(ch >='A && ch <='Z){
+		if((ch >='a' && ch<='z')||(ch >='A && ch <='Z')){
 			System.out.println("Alphabet");
 		}
 		else{
