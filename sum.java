dd209d34cb78afe9c6869611f9c2b49f9460c09a
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-20 13:52:37 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class Eigthkid {
+
+	public static void main(String[] args) {
+		Scanner word=new Scanner(System.in);
+		int N=word.nextInt();int sum=0;
+		for(int i=1;i<=N;i++){
+		  sum+=i;
+		}
+		System.out.println(sum);
+	}
+}
