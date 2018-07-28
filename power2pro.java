16edbaab5d7ce7e7a7db01a2c86e5ce934b94b16
PriyadharshiniSSekar <41110947+PriyadharshiniSSekar@users.noreply.github.com>
2018-07-28 18:57:45 +0530

    Create power2pro.java

---
+++ power2pro.java
@@ -0,0 +1,11 @@
+public class power2pro {
+
+	public static void main(String[] args) {
+       Scanner word=new Scanner(System.in);
+       int num=word.nextInt();
+       if(num%2!=0)
+    	   System.out.println("1");
+       else
+    	   System.out.println("0");
+	}
+}
